aeac5709bfa8e7ace512dfd509e670176790b263
Sayan <sayandas987123654@gmail.com>
2024-05-03 02:04:21 +0530

    modified: index.js

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ document.querySelectorAll("img")[0].setAttribute("src", randomImageSource);
 
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-var randomImageSource2 = "images/dice" + randomDiceImage;
-document.querySelectorAll("img")[1].setAttribute("src", randomImageSource);
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 
 if(randomNumber1 > randomNumber2){
