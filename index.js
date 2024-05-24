dce994b8708cb3fb7893f1e035178578af6d0fc6
Sayan-Rajak-Das <sayandas987123654@gmail.com>
2024-05-25 01:54:52 +0530

    Prevent form refresh on submit (Part 2)

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
+document.getElementById('fetchForm').addEventListener('submit', submitFormFetch);
+
 function submitFormFetch(event) {
-    var fetchForm = document.getElementById("fetchForm");
-    var data = new FormData(fetchForm);
-    fetch("", {
-          method: "post",
-          body: data
-       })
-       .then((res) => {
-          return res.text();
-       })
-       .then((txt) => {
-          alert("Submit Success");
-       })
-       .catch((err) => {
-          alert(err);
-       });
-    return false;
+    // var fetchForm = document.getElementById("fetchForm");
+    // var data = new FormData(fetchForm);
+    // fetch("", {
+    //       method: "post",
+    //       body: data
+    //    })
+    //    .then((res) => {
+    //       return res.text();
+    //    })
+    //    .then((txt) => {
+    //       alert("Submit Success");
+    //    })
+    //    .catch((err) => {
+    //       alert(err);
+    //    });
+    // return false;
+
+
+    
+    event.preventDefault();
+
+    var fetchForm = document.getElementById('fetchForm');
+            fetchForm.reset();
  }
+ 
