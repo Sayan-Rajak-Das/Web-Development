375aa83fed74db442f7571ce6d20b2181d07f4e3
Sayan-Rajak-Das <sayandas987123654@gmail.com>
2024-06-01 01:04:42 +0530

    add a background image and fix some bug

--- index.js
+++ index.js
@@ -42,16 +42,21 @@ document.getElementById('weatherForm').addEventListener('submit', function(event
           return response.json();
       })
       .then(data => {
-          const weatherDescription = `City: ${data.name}, ${data.sys.country}\n` +
-                                     `Temperature: ${data.main.temp}°C\n` +
-                                     `Feels Like: ${data.main.feels_like}°C\n` +
-                                     `Min Temperature: ${data.main.temp_min}°C\n` +
-                                     `Max Temperature: ${data.main.temp_max}°C\n` +
-                                     `Weather: ${data.weather[0].description}\n` +
-                                     `Humidity: ${data.main.humidity}%\n` +
-                                     `Wind Speed: ${data.wind.speed} m/s\n` +
-                                     `Cloudiness: ${data.clouds.all}%\n` +
-                                     `Pressure: ${data.main.pressure} hPa`;
+        const tempCelsius = (data.main.temp - 273.15).toFixed(2);            //Kelvin to Celcius
+        const feelsLikeCelsius = (data.main.feels_like - 273.15).toFixed(2); //Kelvin to Celcius
+        const tempMinCelsius = (data.main.temp_min - 273.15).toFixed(2);     //Kelvin to Celcius
+        const tempMaxCelsius = (data.main.temp_max - 273.15).toFixed(2);     //Kelvin to Celcius
+
+        const weatherDescription = `City: ${data.name}, ${data.sys.country}\n` +
+                                   `Temperature: ${tempCelsius}°C\n` +
+                                   `Feels Like: ${feelsLikeCelsius}°C\n` +
+                                   `Min Temperature: ${tempMinCelsius}°C\n` +
+                                   `Max Temperature: ${tempMaxCelsius}°C\n` +
+                                   `Weather: ${data.weather[0].description}\n` +
+                                   `Humidity: ${data.main.humidity}%\n` +
+                                   `Wind Speed: ${data.wind.speed} m/s\n` +
+                                   `Cloudiness: ${data.clouds.all}%\n` +
+                                   `Pressure: ${data.main.pressure} hPa`;
           document.getElementById('weatherReport').value = weatherDescription;
       })
       .catch(error => {
